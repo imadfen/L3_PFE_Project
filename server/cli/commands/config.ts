f8931f6f81ca3970394c0d087feebfdeb01b07e7
imadfen <imadfen20@gmail.com>
2024-05-18 12:28:32 +0100

    remove the ability to edit lastScanDate config

--- server/cli/commands/config.ts
+++ server/cli/commands/config.ts
@@ -5,7 +5,7 @@ export async function configCommand(commandArgs: string, cb: ()=>any) {
     var regex = /(--\S+)\s+(\S+)\s+("[^"]*"|'[^']*')|(--ls|--list)/;
     var matches = commandArgs.match(regex);
 
-    if (!matches) {
+    ifstatement: if (!matches) {
         console.log("Invalid options.");
     } else if (matches[4]) {
         const config = await getConfigJson();
@@ -20,6 +20,11 @@ export async function configCommand(commandArgs: string, cb: ()=>any) {
     } else {
         const property = matches[2];
         const value = matches[3].slice(1, -1);
+
+        if (property === "lastScanDate") {
+            console.log("You cannot update this config property manually.");
+            break ifstatement;
+        }
 
         const result = await changeConfig(property, value);
 
